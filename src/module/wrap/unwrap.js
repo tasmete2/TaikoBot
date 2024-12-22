d5967dcef215e8873693e17b4b0bbfbd1040dc05
TastasMete <128635711+TastasMete@users.noreply.github.com>
2024-12-22 14:01:21 +0300

    Update and rename wrap.js to unwrap.js

---
+++ src/module/wrap/unwrap.js
@@ -0,0 +1,45 @@
+require('dotenv').config();
+const { web3, walletAddress, privateKey } = require('../../../config/web3');
+const AppConstant = require('../../utils/constant');
+
+const contractABI = [
+    {
+        "constant": false,
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "amount",
+                "type": "uint256"
+            }
+        ],
+        "name": "withdraw", 
+        "outputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    }
+];
+
+const contract = new web3.eth.Contract(contractABI, AppConstant.wrap);
+
+async function unwrap(amount, gasPrice, nonce) {
+    const amountWei = web3.utils.toWei(amount.toString(), 'ether'); 
+    const tx = {
+        from: walletAddress,
+        to: AppConstant.wrap,
+        gas: AppConstant.maxGas,
+        gasPrice: gasPrice,
+        data: contract.methods.withdraw(amountWei).encodeABI(),
+        nonce: nonce,
+        chainId: 167000
+    };
+
+    const signedTx = await web3.eth.accounts.signTransaction(tx, privateKey);
+    const receipt = await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
+    
+    return receipt.transactionHash;
+}
+
+module.exports = {
+    unwrap
+};
