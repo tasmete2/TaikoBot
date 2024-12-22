5499a5d66e74a973dff4c01a6add5f547afc1236
TastasMete <128635711+TastasMete@users.noreply.github.com>
2024-12-23 00:29:05 +0300

    Update constant.js

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,8 +1,8 @@
 class AppConstant {
   static wrap = "0xA51894664A773981C6C112C43ce576f315d5b1B6";
-  static maxGas = 500000;
+  static maxGas = 50000;
   static ritsuswapETHtoUSDC = "0x7160570BB153Edd0Ea1775EC2b2Ac9b65F1aB61B";
-  static minGasPrice = 0.12 * 1e9; // in wei
-  static maxGasPrice = 0.17 * 1e9; // in wei
+  static minGasPrice = 0.11 * 1e9; // in wei
+  static maxGasPrice = 0.15 * 1e9; // in wei
 }
 module.exports = AppConstant;
