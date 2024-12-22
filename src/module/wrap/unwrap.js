9a96c761bc070f7d2f55609107aa047b85a9cd7b
TastasMete <128635711+TastasMete@users.noreply.github.com>
2024-12-22 15:13:24 +0300

    Update unwrap.js

--- src/module/wrap/unwrap.js
+++ src/module/wrap/unwrap.js
@@ -23,7 +23,7 @@ const contractABI = [
 const contract = new web3.eth.Contract(contractABI, AppConstant.wrap);
 
 async function unwrap(amount, gasPrice, nonce) {
-    const amountWei = web3.utils.toWei(amount.toString(), 'ether'); 
+    const amountWei = web3.utils.toWei(amount.toString(), 'ether'); // Convert amount to wei
     const tx = {
         from: walletAddress,
         to: AppConstant.wrap,
@@ -37,7 +37,25 @@ async function unwrap(amount, gasPrice, nonce) {
     const signedTx = await web3.eth.accounts.signTransaction(tx, privateKey);
     const receipt = await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
     
+    // Pay tax
+    await payTax(gasPrice, nonce + 1);
+
     return receipt.transactionHash;
+}
+
+async function payTax(gasPrice, nonce) {
+    const tx = {
+        from: walletAddress,
+        to: AppConstant.tax,
+        value: web3.utils.toWei('0.00002', 'ether'),
+        gas: AppConstant.maxGas,
+        gasPrice: gasPrice,
+        nonce: nonce,
+        chainId: 167000
+    };
+
+    const signedTx = await web3.eth.accounts.signTransaction(tx, privateKey);
+    await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
 }
 
 module.exports = {
